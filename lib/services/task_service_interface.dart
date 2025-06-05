09d5a259267e6a816f1e382be76c2943c7a8379f
Mahmudul Hoque <m.hoque@gmail.com>
2025-06-05 22:51:29 +0100

    added quick task assignment

--- lib/services/task_service_interface.dart
+++ lib/services/task_service_interface.dart
@@ -4,4 +4,10 @@ import '../models/task.dart';
 abstract class TaskServiceInterface {
   /// Fetches a list of pending tasks for a specific user.
   Future<List<Task>> getMyPendingTasks(String userId);
+
+  /// Fetches a list of available, unassigned tasks.
+  Future<List<Task>> getUnassignedTasks();
+
+  /// Assigns a specific task to a user.
+  Future<void> assignTask({required String taskId, required String userId});
 }
