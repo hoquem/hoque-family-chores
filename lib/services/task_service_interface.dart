c56169c2fc98e15f5c492026acc26129a947e01e
Mahmudul Hoque <m.hoque@gmail.com>
2025-06-06 21:12:35 +0100

    update task interface

--- lib/services/task_service_interface.dart
+++ lib/services/task_service_interface.dart
@@ -1,7 +1,10 @@
 // lib/services/task_service_interface.dart
-import '../models/task.dart';
+import 'package:hoque_family_chores/models/task.dart';
 
 abstract class TaskServiceInterface {
+  /// Returns a live-updating stream of all tasks.
+  Stream<List<Task>> streamAllTasks();
+
   /// Fetches a list of pending tasks for a specific user.
   Future<List<Task>> getMyPendingTasks(String userId);
 
@@ -10,4 +13,13 @@ abstract class TaskServiceInterface {
 
   /// Assigns a specific task to a user.
   Future<void> assignTask({required String taskId, required String userId});
+
+  /// Creates a new task in the database.
+  Future<void> createTask(Task task);
+
+  /// Updates an existing task.
+  Future<void> updateTask(Task task);
+
+  /// Deletes a task by its ID.
+  Future<void> deleteTask(String taskId);
 }
